920b71c1eed6c7c718cf56a36f5635793ca62b36
dragonexpert <arceus@teamdimensional.net>
2021-02-16 16:37:56 -0600

    Add files via upload

--- inc/languages/english/admin/socialgroups.lang.php
+++ inc/languages/english/admin/socialgroups.lang.php
@@ -1,1 +1,32 @@
-
+<?php
+/**
+ * Socialgroups plugin created by Mark Janssen.
+ * This is not a free plugin.
+ */
+$l['socialgroup'] = "Social Group";
+$l['social_groups'] = "Social Groups";
+$l['admin_log_user_social_groups_manage_group'] = "Edited Social Group # {2} ( {3} )";
+$l['admin_log_user_social_groups_manage_categories_add_category'] = "Created Social Group Category # {2} ( {3} )";
+$l['admin_log_user_social_groups_manage_categories_edit_category'] = "Edited Social Group Category # {2} ( {3} )";
+$l['admin_log_user_social_groups_create_group'] = "Created Social Group # {2} ( {3} )";
+$l['admin_log_user_social_groups_update_cache'] = "Updated Social Group Caches";
+$l['socialgroups_mycode'] = "Allow My Code";
+$l['socialgroups_allow_mycode'] = "Choose to allow mycode to be parsed or not.";
+$l['allow_groups_html'] = "Allow HTML";
+$l['allow_html_groups'] = "Choose to allow HTML to be parsed or not.";
+$l['groups_groups'] = "Groups";
+$l['socialgroups_info'] = "Allows you to have control over social groups";
+$l['groups_group'] = "Groups";
+$l['allow_groups_html_info'] = "Choose if HTML should be parsed.";
+$l['allow_imgcode_groups'] = "Allow images in posts.";
+$l['allow_imgcode_groups_info'] = "Allows you to choose if images should be allowed in posts.";
+$l['allow_videocode_groups'] = "Allow videos in posts.";
+$l['allow_videocode_groups_info'] = "Allows you to choose if videos should be allowed in posts.";
+$l['filter_badwords_groups'] = "Filter bad words?";
+$l['filter_badwords_groups_info'] = "Allows you to choose if bad words should be changed to their replacement.";
+$l['socialgroups_info'] = "Allows you to have control over social groups";
+$l['can_create_social_groups'] = "Can create Social Groups?";
+$l['socialgroups_max_create'] = "Max Social Groups Creation";
+$l['socialgroups_max_create_description'] = "The maximum number of social groups members of this usergroup can create.  Enter 0 for unlimited.";
+$l['socialgroups_auto_approve'] = "Automatically Approve Social Groups?";
+$l['socialgroups_auto_approve_description'] = "Members in this usergroup do not need to have their group approved by a moderator.";
