ed9238e2b0c4c87c2a87c17497ab48ee4488bdda
dragonexpert <arceus@teamdimensional.net>
2021-02-16 16:37:08 -0600

    Add files via upload

---
+++ inc/plugins/socialgroups.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Socialgroups plugin created by Mark Janssen.
+ * This is not a free plugin.
+ */
+if(!defined("IN_MYBB"))
+{
+    die("Direct access not allowed.");
+}
+require_once "socialgroups/hooks.php";
+
+
+function socialgroups_info()
+{
+    return array(
+        "name" => "Social Groups",
+        "description" => "Allows users to create their own groups.",
+        "author" => "Mark Janssen",
+        "version" => "1.8.0",
+        "codename" => "socialgroups"
+    );
+}
+
+
+function socialgroups_install()
+{
+    require_once "socialgroups/db.php";
+    socialgroups_create_tables();
+    require_once "socialgroups/settings.php";
+    socialgroups_insert_settings();
+}
+
+function socialgroups_is_installed()
+{
+    global $db;
+    return $db->table_exists("socialgroups");
+}
+
+function socialgroups_activate()
+{
+    require_once "socialgroups/templates.php";
+    socialgroups_insert_templates();
+}
+
+function socialgroups_deactivate()
+{
+    require_once "socialgroups/templates.php";
+    socialgroups_delete_templates();
+}
+
+function socialgroups_uninstall()
+{
+    require_once "socialgroups/db.php";
+    socialgroups_drop_tables();
+    require_once "socialgroups/settings.php";
+    socialgroups_delete_settings();
+}
