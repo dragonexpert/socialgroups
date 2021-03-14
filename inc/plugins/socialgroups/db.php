455a19d25527e50dc0f8a70614f815859f7d4017
dragonexpert <arceus@teamdimensional.net>
2021-03-14 18:51:53 -0500

    Closes #46
    Closes #46

--- inc/plugins/socialgroups/db.php
+++ inc/plugins/socialgroups/db.php
@@ -108,7 +108,7 @@ $tables['socialgroup_members'] = array(
         "default" => 1
     ),
     "dateline" => array(
-        "type" => "BIGINT",
+        "type" => "INT",
         "default" => 0
     )
 );
@@ -565,132 +565,12 @@ function socialgroups_generate_table_sql(string $table_name, $array=array())
 
 function socialgroups_create_tables()
 {
-    global $db, $cache, $mybb, $socialgroups;
-    $charset = $db->build_create_table_collation();
-
-    // TODO run the actual arrays through the function.
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroups (
-    gid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    cid INT NOT NULL DEFAULT 1,
-    name TEXT,
-    approved INT NOT NULL DEFAULT 1,
-    description TEXT,
-    logo TEXT,
-    private INT NOT NULL DEFAULT 0,
-    staffonly INT NOT NULL DEFAULT 0,
-    inviteonly INT NOT NULL DEFAULT 0,
-    jointype INT NOT NULL DEFAULT 0,
-    uid INT NOT NULL DEFAULT 1,
-    threads INT NOT NULL DEFAULT 0,
-    posts INT NOT NULL DEFAULT 0,
-    locked INT NOT NULL DEFAULT 0
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_categories (
-    cid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    disporder INT NOT NULL DEFAULT 1,
-    name TEXT,
-    groups INT NOT NULL DEFAULT 0,
-    staffonly INT NOT NULL DEFAULT 0
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_members (
-    mid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    gid INT NOT NULL DEFAULT 1,
-    uid INT NOT NULL DEFAULT 1,
-    dateline BIGINT NOT NULL DEFAULT 0
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_member_permissions (
-    pid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    gid INT NOT NULL DEFAULT 1,
-    postthreads INT NOT NULL DEFAULT 1,
-    postreplies INT NOT NULL DEFAULT 1,
-    inviteusers INT NOT NULL DEFAULT 1,
-    deleteposts INT NOT NULL DEFAULT 0
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_moderators (
-    mid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    uid INT NOT NULL DEFAULT 1
-    ) ENGINE = Innodb $charset");
-
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_leaders (
-    lid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    uid INT NOT NULL DEFAULT 1,
-    gid INT NOT NULL DEFAULT 1
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_invites (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    gid INT NOT NULL DEFAULT 1,
-    touid INT NOT NULL DEFAULT 1,
-    fromuid INT NOT NULL DEFAULT 1
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_announcements (
-    aid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    gid INT NOT NULL DEFAULT 1,
-    uid INT NOT NULL DEFAULT 1,
-    dateline BIGINT NOT NULL DEFAULT 0,
-    subject TEXT,
-    message TEXT,
-    active INT NOT NULL DEFAULT 1
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_threads (
-    tid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    gid INT NOT NULL DEFAULT 1,
-    firstpost INT NOT NULL DEFAULT 0,
-    uid INT NOT NULL DEFAULT 1,
-    subject TEXT,
-    dateline BIGINT NOT NULL DEFAULT 0,
-    sticky INT NOT NULL DEFAULT 0,
-    visible INT NOT NULL DEFAULT 1,
-    closed INT NOT NULL DEFAULT 0,
-    replies INT NOT NULL DEFAULT 0,
-    views INT NOT NULL DEFAULT 0,
-    lastposttime BIGINT NOT NULL DEFAULT 0,
-    lastpostuid INT NOT NULL DEFAULT 1,
-    lastpostusername TEXT
-    ) ENGINE = Innodb$charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_posts (
-    pid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tid INT NOT NULL DEFAULT 1,
-    gid INT NOT NULL DEFAULT 1,
-    uid INT NOT NULL DEFAULT 1,
-    dateline BIGINT NOT NULL DEFAULT 0,
-    lastedit BIGINT NOT NULL DEFAULT 0,
-    lasteditby INT NOT NULL DEFAULT 0,
-    editcount INT NOT NULL DEFAULT 0,
-    reported INT NOT NULL DEFAULT 0,
-    visible INT NOT NULL DEFAULT 1,
-    ipaddress VARBINARY(16) NOT NULL DEFAULT '',
-    message TEXT
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_reported_posts (
-    rid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    pid INT NOT NULL DEFAULT 0,
-    tid INT NOT NULL DEFAULT 0,
-    uid INT NOT NULL DEFAULT 0,
-    dateline BIGINT NOT NULL DEFAULT 0,
-    reason TEXT,
-    status TINYINT(1) NOT NULL DEFAULT 0,
-    handledby INT NOT NULL DEFAULT 0,
-    handledate BIGINT NOT NULL DEFAULT 0
-    ) ENGINE = Innodb $charset");
-
-    $db->query("CREATE TABLE IF NOT EXISTS " . TABLE_PREFIX . "socialgroup_join_requests (
-    rid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    gid INT NOT NULL DEFAULT 0,
-    uid INT NOT NULL DEFAULT 0,
-    dateline BIGINT NOT NULL DEFAULT 0,
-    approved INT NOT NULL DEFAULT 0
-    ) ENGINE = Innodb $charset");
+    global $db, $cache, $mybb, $socialgroups, $tables;
+    foreach($tables as $tablename => $definition)
+    {
+        $result = socialgroups_generate_table_sql($tablename, $definition);
+        $db->query($result);
+    }
 
     // Create a category.  We are setting groups to 1 here because we are creating a group automatically.
     $new_category = array(
@@ -715,30 +595,21 @@ function socialgroups_create_tables()
         "uid" => (int) $mybb->user['uid']
     );
 
-    $gid = $db->insert_query("socialgroups", $new_group);
-
     if(!is_object($socialgroups))
     {
         require_once "classes/socialgroups.php";
         $socialgroups = new socialgroups();
     }
-    $socialgroups->socialgroupsuserhandler->join($gid, $mybb->user['uid'], 1);
 
-    $new_leader = array(
-        "gid" => $gid,
-        "uid" => $mybb->user['uid']
-    );
-
-    $db->insert_query("socialgroup_leaders", $new_leader);
-
-    $socialgroups->update_cache();
-    $socialgroups->update_socialgroups_category_cache();
+    $socialgroups->socialgroupsdatahandler->save($new_group, "insert");
 
     // Usergroup permissions
-    $db->write_query("ALTER TABLE " . TABLE_PREFIX . "usergroups ADD maxsocialgroups_create INT UNSIGNED DEFAULT 5, ADD cancreatesocialgroups INT UNSIGNED DEFAULT 1, ADD socialgroups_auto_approve INT UNSIGNED DEFAULT 0");
-    $db->write_query("UPDATE " . TABLE_PREFIX . "usergroups SET cancreatesocialgroups=0 WHERE isbannedgroup=1");
-    $db->write_query("UPDATE " . TABLE_PREFIX . "usergroups SET maxsocialgroups_create=0 WHERE cancp=1");
-    $db->write_query("UPDATE " . TABLE_PREFIX . "usergroups SET socialgroups_auto_approve=1 WHERE cancp=1 OR canmodcp=1");
+    $db->add_column("usergroups", "maxsocialgroups_create", "INT NOT NULL DEFAULT 5");
+    $db->add_column("usergroups", "cancreatesocialgroups", "INT NOT NULL DEFAULT 1");
+    $db->add_column("usergroups", "socialgroups_auto_approve", "INT NOT NULL DEFAULT 0");
+    $db->update_query("usergroups", array("cancreatesocialgroups" => 0), "isbannedgroup=1");
+    $db->update_query("usergroups", array("maxsocialgroups_create" => 0), "cancp=1");
+    $db->update_query("usergroups", array("socialgroups_auto_approve" => 1), "cancp=1 OR canmodcp=1");
     $cache->update_usergroups();
 }
 
@@ -760,7 +631,7 @@ function socialgroups_drop_tables()
     {
         if($db->field_exists($column, "usergroups"))
         {
-            $db->write_query("ALTER TABLE " . TABLE_PREFIX . "usergroups DROP " . $column);
+            $db->drop_column("usergroups", $column);
         }
     }
     $cache->update_usergroups();
